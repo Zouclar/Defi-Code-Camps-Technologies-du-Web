7768c50232bb822feb37407eacea84389ec1b724
nakach_t <nakach_t@c635fa9f-83ef-4942-98e0-312faf920fd4>
2015-01-29 21:59:02 +0000

    API !!!

--- Defi Code Camps - Technologies du Web/doussi_f/index.js
+++ Defi Code Camps - Technologies du Web/doussi_f/index.js
@@ -60,3 +60,53 @@ $('#chaine').append('<img id="fr2" src="http://static3.programme-tv.com//images/
 
 $('footer').append('<div>').attr('id', 'menu_footer');
 $('#menu_footer').prepend('<a href="http://tivine.com/blog/?p=79"><img id="logo_6" src="./logo6.png" /></a>').prepend('<img id="logo_5" src="./logo5.png" />').prepend('<img id="logo_4" src="./logo4.png" />').prepend('<a href="http://entrepreneurs.telecom-paristech.fr/"><img id="logo_3" src="./logo3.png" /></a>').prepend('<img id="logo_2" src="./logo2.png" />').prepend('<img id="logo_1" src="./logo1.png" />');
+
+$url="http://94.23.253.36:8080/TiVineWS_V1.0/SearchStampBeyonce10989841";
+$key = "8de53b9a88725ff0d9357431b486f7ac";
+$hash = CryptoJS.HmacSHA512($url, "8de53b9a88725ff0d9357431b486f7ac").toString();
+function searchStamp() 
+{
+	console.log($hash);
+	$.ajax({
+//		xhrFields: { withCredentials: true }
+		url: "http://94.23.253.36:8080/TiVineWS_V1.0/SearchStamp",
+		type: "POST",
+		data: {'clientId':"10989841",'query':"Beyonce",'encodedKey':$hash},
+		success:function(data)
+		{
+			console.log(data.stamps[0].name);
+			console.log(data.stamps[0].image);
+		},
+		error:function() 
+		{
+			alert("error");
+		}
+	});
+}
+
+
+function ChannelContent(jour,chaine) 
+{
+	
+	$url="http://94.23.253.36:8080/TiVineWS_V1.0/GetChannelContentForDay20150129110989841";
+	$hash = CryptoJS.HmacSHA512($url, "8de53b9a88725ff0d9357431b486f7ac").toString();
+	console.log($hash);
+	$.ajax({
+//		xhrFields: { withCredentials: true }
+		url: "http://94.23.253.36:8080/TiVineWS_V1.0/GetChannelContentForDay",
+		type: "POST",
+		data: {'day':jour,'channel':chaine,'clientId':"10989841",'encodedKey':$hash},
+		success:function(data)
+		{
+			$titre=(data.programs[1].title);
+			$image=(data.programs[1].image);
+			console.log($titre);
+			console.log($image);
+			$('#photos').append('<div>').attr('id','lol')
+		},
+		error:function() 
+		{
+			alert("error");
+		}
+	});
+}
